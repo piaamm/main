793cff8c9ee7ffe44147fd0cbf87c86f8c2c9972
piaamm <116604725+piaamm@users.noreply.github.com>
2022-12-20 16:55:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("contextmenu", (event) => event.preventDefault());
 
 var started = false;
 
-var audio = new Audio("shadowlady.mp3");
+var audio = new Audio("Twizzy Rich.mp3");
 function play() {
 	started = !started;
 	if (started) {
